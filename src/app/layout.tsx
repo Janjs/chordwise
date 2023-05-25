48cd7ea5d20d1d3ba437e61f951be3c604a987ce
Jan Jiménez Serra <jan.jime.serra@gmail.com>
2023-05-25 21:26:51 +0200

    add footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,11 @@ export default function RootLayout({
       <body className={inter.className}>
         <Header />
         {children}
+        <p className="fixed bottom-0 right-0 pr-14 pb-8 text-muted-foreground">
+          Made by <a href="https://github.com/Janjs" className="text-foreground">@Janjs</a>
+          <br />
+          Powered by ChatGPT
+        </p>
       </body>
     </html>
   );
