5cda928892abd1b90edfcaf15e02c8946947be6e
Jan J. Serra <jan.jime.serra@gmail.com>
2023-10-08 16:03:42 +0200

    update accordion to not close other on press

--- components/player-viewer.tsx
+++ components/player-viewer.tsx
@@ -9,24 +9,21 @@ interface PlayerViewerProps<GuitarChordProgViewerProps> {
 }
 
 const PlayerViewer: FC<PlayerViewerProps<GuitarChordProgViewerProps>> = ({ guitarChordProgViewerProps }) => {
-  const [openItems, setOpenItems] = useState<string[]>()
+  const [openItems, setOpenItems] = useState<string[]>([])
 
-  const handleValueChange = (value: string) => {
+  function handleItemClick(id: string) {
     setOpenItems((prev) => {
-      if (prev!!.includes(value)) {
-        // If the item is already open, close it
-        return prev.filter((item) => item !== value)
+      if (prev.includes(id)) {
+        return prev.filter((item) => item !== id)
       } else {
-        // Otherwise, add the item to the open items
-        return [...prev, value]
+        return [...prev, id]
       }
     })
   }
-
   return (
     <div className="flex-1 rounded-xl p-5 flex flex-row bg-card">
       <div className="w-full">
-        <Accordion type="single" defaultValue="guitar" collapsible>
+        <Accordion type="multiple" defaultValue={['guitar', 'piano']}>
           <AccordionItem value="guitar">
             <AccordionTrigger>Guitar</AccordionTrigger>
             <AccordionContent>
